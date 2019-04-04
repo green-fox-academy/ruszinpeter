0f8cd42ba2f229f2658beeac5304d95aec858fff
Peter Ruszin <ruszin.peter@gmail.com>
2019-04-04 15:59:43 +0200

    correct working hours and average hours

--- week-01/day-3/types.ts
+++ week-01/day-3/types.ts
@@ -13,5 +13,8 @@ console.log(22%13);
 console.log('Working Hours');
 console.log('daily coding hours: 6');
 console.log('semester length: 17 weeks');
+
 console.log('average work hours weekly: 52');
-console.log('Hours with spent coding: ' + 17*52);
+console.log('Hours with spent coding: '+ 6*5*17 );
+
+console.log('Percentage of coding hours in the semester: ' + (30*17)/(52*17)*100);
