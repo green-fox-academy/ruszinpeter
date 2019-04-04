54c1b9d4eae6735ed06d62ae578d59e3304f07bc
Peter Ruszin <ruszin.peter@gmail.com>
2019-04-04 13:51:04 +0200

    creating humpty-dumpty.ts

--- week-01/day-3/humpty-dumpty.ts
+++ week-01/day-3/humpty-dumpty.ts
@@ -1,5 +1,5 @@
 console.log('Humpty Dumpty sat on a wall,');
 console.log('Humpty Dumpty had a great fall.');
 console.log('All the king\'s horses and all the king\'s men');
-console.log('Couldn\'t put Humpty together again.');
+console.log('Couldn\'t put Humpty together again..');
 
