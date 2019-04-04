4574ba03a7b1b9b87c44dabb018d3aac9372fbf5
Peter Ruszin <ruszin.peter@gmail.com>
2019-04-04 16:34:07 +0200

    add basic info task

--- week-01/day-3/exercises-with-let.ts
+++ week-01/day-3/exercises-with-let.ts
@@ -5,11 +5,18 @@ let favoriteNumber: number = 8;
 console.log('My favorite number is: ' + favoriteNumber);
 
 //swap exercise
-let a: number = 526;
-let b: number = 123;
+let a: number = 123;
+let b: number = 526;
 
 console.log(a);
 console.log(b);
+
+a = 526
+b = 123
+
+console.log(a);
+console.log(b);
+
 
 //bmi
 
@@ -18,3 +25,12 @@ let heightInM: number = 1.78;
 
 let BMI: number = massInKg / (heightInM * heightInM); 
 console.log('BMI : '+ BMI); 
+
+//basic info about myself
+
+let myName: string = 'Peter';
+let age: number = 36;
+let height: number = 1.75;
+let marriedOrNot: boolean = false;
+
+console.log(myName, age, height, marriedOrNot);
