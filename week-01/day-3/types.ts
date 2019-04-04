63f7acb458a5c0309e9e1ae9d943eee2d08e1c81
Peter Ruszin <ruszin.peter@gmail.com>
2019-04-04 15:34:06 +0200

    add Types/Introduce/two-numbers/coding-hours exercises/

---
+++ week-01/day-3/types.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+console.log('Peter Ruszin');
+console.log('36');
+console.log('1,75');
+
+console.log(22 + 13);
+console.log(13 - 22);
+console.log(22*13);
+console.log(22/13);
+console.log(22%13);
+
+console.log('Working Hours');
+console.log('daily coding hours: 6');
+console.log('semester length: 17 weeks');
+console.log('average work hours weekly: 52');
+console.log('Hours with spent coding: ' + 17*52);
