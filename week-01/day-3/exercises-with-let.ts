36aa3dc2645fc3fc5803ae194976dab08f8502c3
Peter Ruszin <ruszin.peter@gmail.com>
2019-04-04 16:23:32 +0200

    add fav-number/swap/bmi exercises

---
+++ week-01/day-3/exercises-with-let.ts
@@ -0,0 +1,20 @@
+'use strict';
+// favorite-number
+
+let favoriteNumber: number = 8;
+console.log('My favorite number is: ' + favoriteNumber);
+
+//swap exercise
+let a: number = 526;
+let b: number = 123;
+
+console.log(a);
+console.log(b);
+
+//bmi
+
+let massInKg: number = 81.2;
+let heightInM: number = 1.78;
+
+let BMI: number = massInKg / (heightInM * heightInM); 
+console.log('BMI : '+ BMI); 
