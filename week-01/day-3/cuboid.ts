543484363dae75755d9909a56a829005cba05acd
Peter Ruszin <ruszin.peter@gmail.com>
2019-04-05 10:00:32 +0200

    add cuboid surface and volume

---
+++ week-01/day-3/cuboid.ts
@@ -0,0 +1,11 @@
+'use strict'
+
+let sideA: number= 50;
+let sideB: number = 20;
+let sideC: number = 10;
+
+let surfaceArea = 2*(sideA*sideB)+2*(sideA*sideC)+2*(sideB*sideC);
+let cuboidVolume = sideA*sideB*sideC;
+
+console.log(surfaceArea);
+console.log(cuboidVolume);
