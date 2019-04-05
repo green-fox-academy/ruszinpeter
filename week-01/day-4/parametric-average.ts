484da3aae26eefaaffe8072e76e772a8e3c95ccc
Peter Ruszin <ruszin.peter@gmail.com>
2019-04-05 12:36:22 +0200

    add parametric avergage

---
+++ week-01/day-4/parametric-average.ts
@@ -0,0 +1,10 @@
+'use strict'
+
+let a: number = 15;
+let sumOfNumbers: number = a;
+
+for (let i: number = 0; i <= a; i++) {
+    sumOfNumbers = sumOfNumbers + i;
+   }
+   console.log(sumOfNumbers);
+
