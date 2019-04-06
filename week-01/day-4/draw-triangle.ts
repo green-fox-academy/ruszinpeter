2cd413d48058863886c85bdc5cd46213f5e172da
Peter Ruszin <ruszin.peter@gmail.com>
2019-04-06 11:43:21 +0200

    created draw-triangle task

---
+++ week-01/day-4/draw-triangle.ts
@@ -0,0 +1,10 @@
+'use strict'
+
+let lineCount: number = 11;
+let drawTriangle: string = "*";
+
+for (let i:number = 1; i<=lineCount; i++) {
+    console.log(drawTriangle);
+    drawTriangle = drawTriangle + "*";
+}
+
