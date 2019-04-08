9e64c9c145ca35bd9248b918a6745f97eec889d7
Peter Ruszin <ruszin.peter@gmail.com>
2019-04-08 15:18:22 +0200

    add third.ts task

---
+++ week-02/day-1/print-params.ts
@@ -0,0 +1,7 @@
+'use strict'
+
+// -  Create a function called `printParams`
+//    which logs to the console the input parameters
+//    (can have multiple number of arguments)
+
+function(printParams)
