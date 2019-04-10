a9848893d8be4e2a2ae3feb1839ae8939d44cec8
Peter Ruszin <ruszin.peter@gmail.com>
2019-04-10 15:49:03 +0200

    add rainbow-box-function task

--- week-02/day-3/sample.ts
+++ week-02/day-3/sample.ts
@@ -3,7 +3,7 @@
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 canvas.height = 600;
 canvas.width = 600;
-const ctx = canvas.getContext('2d')
+
 
  if (ctx !== null) {
   ctx.fillStyle = 'grey';
