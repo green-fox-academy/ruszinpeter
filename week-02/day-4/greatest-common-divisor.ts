398945f5974aed5f6c369a4fb9479003aa8792f1
Peter Ruszin <ruszin.peter@gmail.com>
2019-04-11 14:50:30 +0200

    created greatest common divisor task

---
+++ week-02/day-4/greatest-common-divisor.ts
@@ -0,0 +1,9 @@
+'use strict'
+
+/*Find the greatest common divisor of two numbers using recursion.*/
+
+function commonDivisor(a: number, b: number): number {
+    if (a % b == 0) return b;
+    return commonDivisor(b, a % b);
+}
+console.log(commonDivisor(42, 105));
